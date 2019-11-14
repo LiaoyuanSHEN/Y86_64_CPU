3de3121aaae3161e851fa257d7ae8ce5bdb781fb
root <root@Suit-PC.localdomain>
2019-11-14 23:47:02 +0800

    first draft

---
+++ src/main/java/y86_64/Const.java
@@ -0,0 +1,28 @@
+package y86_64;
+
+public class Const {
+
+    // RF
+    public static int RAX = 0;
+    public static int RBX = 1;
+    public static int RCX = 2;
+    public static int RDX = 3;
+    public static int RSP = 4;
+    public static int RBP = 5;
+    public static int RSI = 6;
+    public static int RDI = 7;
+    public static int R08 = 8;
+    public static int R09 = 9;
+    public static int R10 = 10;
+    public static int R11 = 11;
+    public static int R12 = 12;
+    public static int R13 = 13;
+    public static int R14 = 14;
+    public static int R15 = 15;
+
+    // CC
+    public static int ZF = 0;
+    public static int SF = 1;
+    public static int QF = 2;
+
+}
