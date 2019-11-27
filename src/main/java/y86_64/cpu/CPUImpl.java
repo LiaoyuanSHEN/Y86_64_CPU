14910ab7dd16b2ca85ce1f94773ca8cbc25fe148
Liaoyuan SHEN <liaoyuan.shen@outlook.com>
2019-11-27 23:21:14 +0800

    draft

--- src/main/java/y86_64/cpu/CPUImpl.java
+++ src/main/java/y86_64/cpu/CPUImpl.java
@@ -3,15 +3,18 @@ package y86_64.cpu;
 import y86_64.CPU;
 import y86_64.Memory;
 import y86_64.exceptions.CpuException;
+import y86_64.exceptions.MemoryException;
 
 import java.io.IOException;
+
+import static y86_64.cpu.Const.*;
 
 public class CPUImpl implements CPU {
 
     private final long[] registers = new long[16];
     private final boolean[] flags = new boolean[3];
     private long pc = 0;
-    private int status = 0;
+    private int status = READY_CODE;
     private final Memory memory;
 
     public CPUImpl(Memory memory) {
@@ -20,7 +23,44 @@ public class CPUImpl implements CPU {
 
     @Override
     public void compute() throws CpuException {
-
+        int operationCode = (int) readNext();
+        int registerIndex1;
+        int registerIndex2;
+        long memoryOffset;
+        long memoryAddress;
+        switch (operationCode) {
+            case nop:
+                break;
+            case halt:
+                status = HALT_CODE;
+                break;
+            case irmoveq:
+                registerIndex1 = (int) readNext();
+                long value = readNext();
+                registers[registerIndex1] = value;
+                break;
+            case rrmoveq:
+                registerIndex1 = (int) readNext();
+                registerIndex2 = (int) readNext();
+                registers[registerIndex2] = registerIndex1;
+                break;
+            case mrmoveq:
+                registerIndex1 = (int) readNext();
+                registerIndex2 = (int) readNext();
+                memoryOffset = readNext();
+                memoryAddress = registers[registerIndex2] + memoryOffset;
+                registers[registerIndex1] = readFromMemory(memoryAddress);
+                break;
+            case rmmoveq:
+                registerIndex1 = (int) readNext();
+                registerIndex2 = (int) readNext();
+                memoryOffset = readNext();
+                memoryAddress = registers[registerIndex1] + memoryOffset;
+                writeToMemory(memoryAddress, registers[registerIndex2]);
+                break;
+            default:
+                throw new CpuException("Unknown operation code: " + operationCode);
+        }
     }
 
     @Override
@@ -32,4 +72,29 @@ public class CPUImpl implements CPU {
     public void stop() throws IOException {
         // do nothing
     }
+
+    private void writeToMemory(long address, long value) {
+        try {
+            memory.write(address, value);
+        } catch (MemoryException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private long readFromMemory(long address) {
+        try {
+            return memory.read(address);
+        } catch (MemoryException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private long readNext() {
+        try {
+            return memory.read(pc++);
+        } catch (MemoryException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
 }
--- src/main/java/y86_64/cpu/Const.java
+++ src/main/java/y86_64/cpu/Const.java
@@ -3,26 +3,73 @@ package y86_64.cpu;
 public class Const {
 
     // RF
-    public static int RAX = 0;
-    public static int RBX = 1;
-    public static int RCX = 2;
-    public static int RDX = 3;
-    public static int RSP = 4;
-    public static int RBP = 5;
-    public static int RSI = 6;
-    public static int RDI = 7;
-    public static int R08 = 8;
-    public static int R09 = 9;
-    public static int R10 = 10;
-    public static int R11 = 11;
-    public static int R12 = 12;
-    public static int R13 = 13;
-    public static int R14 = 14;
-    public static int R15 = 15;
+    public final static int RAX = 0;
+    public final static int RBX = 1;
+    public final static int RCX = 2;
+    public final static int RDX = 3;
+    public final static int RSP = 4;
+    public final static int RBP = 5;
+    public final static int RSI = 6;
+    public final static int RDI = 7;
+    public final static int R08 = 8;
+    public final static int R09 = 9;
+    public final static int R10 = 10;
+    public final static int R11 = 11;
+    public final static int R12 = 12;
+    public final static int R13 = 13;
+    public final static int R14 = 14;
+    public final static int R15 = 15;
 
     // CC
-    public static int ZF = 0;
-    public static int SF = 1;
-    public static int QF = 2;
+    public final static int ZF = 0;
+    public final static int SF = 1;
+    public final static int QF = 2;
+
+    // Status
+    public final static int READY_CODE = 0;
+    public final static int RUNNING_CODE = 1;
+    public final static int HALT_CODE = 2;
+
+    // Command
+    /// V stands for a number(immediate)
+    /// r* stands for a register
+    /// D(r*) stands for a value in memory with address equals r*
+    public final static int nop = 0;
+    public final static int halt = 1;
+    // moves
+    // irmoveq V rA -> 10 V (index of rA)
+    public final static int irmoveq = 10;
+    // irmoveq rA rB -> 11 (index of rA) (index of rB)
+    public final static int rrmoveq = 11;
+    // irmoveq D(rA) rB -> 12 (index of rA) (index of rB) (value of D)
+    public final static int mrmoveq = 12;
+    // irmoveq rA D(rB) -> 12 (index of rA) (index of rB) (value of D)
+    public final static int rmmoveq = 13;
+    // operations
+    public final static int addq = 20;
+    public final static int subq = 21;
+    public final static int andq = 22;
+    public final static int xorq = 23;
+    // jumps
+    public final static int jmp = 30;
+    public final static int jle = 31;
+    public final static int jl = 32;
+    public final static int je = 33;
+    public final static int jne = 34;
+    public final static int jge = 35;
+    public final static int jg = 36;
+    // condition moves
+    public final static int cmovle = 40;
+    public final static int cmovl = 41;
+    public final static int cmove = 42;
+    public final static int cmovne = 43;
+    public final static int cmovge = 44;
+    public final static int cmovg = 45;
+    // calls
+    public final static int call = 50;
+    public final static int ret = 51;
+    // stack operations
+    public final static int pushq = 60;
+    public final static int popq = 61;
 
 }
